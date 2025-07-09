2bc2bdc0a12decb71c2514877fbdb23405321d08
carolinewales <112579532+carolinewales@users.noreply.github.com>
2025-07-09 11:48:30 -0400

    Add files via upload

---
+++ completionEmail.py
@@ -0,0 +1,21 @@
+from email.message import EmailMessage
+import smtplib
+import os
+
+
+logFile = 'output.log'
+if not os.path.exists(logFile):
+    output = 'No output.log file found.'
+else:
+    with open(logFile, 'r') as f:
+        output = f.read()
+
+message = EmailMessage()
+message['Subject'] = 'Twin Lion Meta Feed Updated'
+message['From'] = os.environ['EMAIL_ADDRESS']
+message['To'] = os.environ['EMAIL_ADDRESS']
+message.set_content('Successful update complete! You are awesome, have a good day. \n Output:\n\n' + output[:6000])
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(os.environ['EMAIL_ADDRESS'], os.environ['EMAIL_PASSWORD'])
+    smtp.send_message(message)
